f765e532631552bce72938c13ea475c26fe04403
skrt <farm.shk@gmail.com>
2022-01-11 22:23:00 +0700

    lab15

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 template <typename T>
-void insertionSort(T d[],int N){
-
+void insertionSort(T d[],int N)
+{
+  for(int i = 1 ; i < N; i++)
+  {
+  for(int j = 0; j < i; j++)
+  {
+   if(d[i] > d[j])
+   {
+     T val = d[i];
+     d[i] = d[j];
+     d[j] = val;
+   }
+  }
+  cout << "Pass " << i << ":";
+  for (int i = 0; i < N ; i++) cout << d[i] << " ";
+  cout << "\n";
+ }
 }
 
 int main(){
